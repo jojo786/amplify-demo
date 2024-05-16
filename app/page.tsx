2068f251e3a135b93037a2f462c3ac373712ff14
Yusuf Mayet <jojo786@gmail.com>
2024-05-16 09:09:07 +0200

    Deploy to AWS

--- app/page.tsx
+++ app/page.tsx
@@ -98,13 +98,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Deploy to AWS{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Instantly deploy your Next.js site to a shareable URL with AWS.
           </p>
         </a>
       </div>
